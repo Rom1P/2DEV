86012cb1836a50ee9b99f9600147fcfbfef3e5a3
Rom1P <romain.pilotta@gmail.com>
2018-04-08 01:32:25 +0200

    UnityScenes

--- Assets/Scripts/CreateBoard.cs
+++ Assets/Scripts/CreateBoard.cs
@@ -4,13 +4,20 @@ using UnityEngine;
 
 public class CreateBoard : MonoBehaviour {
 
+    public GameObject CasePrefab;
+
 	// Use this for initialization
 	void Start () {
-		
+        LoadBoard();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void LoadBoard()
+    {
+        GameObject casePrefabClone = Instantiate(CasePrefab, transform.position, transform.rotation);
+    }
 }
